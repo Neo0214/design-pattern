8ccf87901118b8d4b73061854ef75dfcad8fb96c
zsmx0323 <2558779606@qq.com>
2024-01-01 23:09:28 +0800

    ddx-02

--- src/main/java/org/group11/Patterns/serviceLocator/CreateUserService.java
+++ src/main/java/org/group11/Patterns/serviceLocator/CreateUserService.java
@@ -1,5 +1,10 @@
 package org.group11.Patterns.serviceLocator;
 
+import org.apache.ibatis.session.SqlSession;
+import org.group11.Patterns.simpleFactory.JDBCFactory;
+import org.group11.Patterns.simpleFactory.UserFactory;
+import org.group11.Tools.Input;
+
 /*
  * @author Neo0214
  */
@@ -11,5 +16,16 @@ public class CreateUserService implements Service{
     @Override
     public void execute(String... args) {
         System.out.println("Executing CreateUserService");
+        SqlSession sqlSession = JDBCFactory.Instance();
+        String username;
+        String password;
+        System.out.println("请输入用户名");
+        username=Input.getInput();
+        System.out.println("请输入密码");
+        password= Input.getInput();
+        assert sqlSession != null;
+        User user=new UserFactory().createUser(sqlSession,username,password);
+        sqlSession.commit();
+        System.out.println("创建成功");
     }
 }
