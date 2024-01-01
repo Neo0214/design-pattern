f431693df7e5838d4ac58442fc5542bf3a483228
Peng <690271528@qq.com>
2024-01-01 12:41:56 +0800

    pwx-01

--- src/main/java/org/group11/Patterns/DAO/Impl/CommentMapperImpl.java
+++ src/main/java/org/group11/Patterns/DAO/Impl/CommentMapperImpl.java
@@ -6,23 +6,24 @@ import org.group11.Patterns.DAO.CommentMapper;
 import org.group11.Patterns.simpleFactory.JDBCFactory;
 import org.group11.Patterns.simpleFactory.ParamFactory;
 
-import java.awt.print.Book;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
-public class CommentMapperImp implements CommentMapper {
+public class CommentMapperImpl implements CommentMapper {
     @Override
     public boolean saveBookComment(int BOOK_ID, LocalDateTime COMMENT_TIME, String COMMENT_CONTENT, int SCORE) {
         // 创建参数
         Map<String,String> params= ParamFactory.getParam("id",String.valueOf(BOOK_ID),"COMMENT_TIME",String.valueOf(COMMENT_TIME),"COMMENT_CONTENT",COMMENT_CONTENT,"SCORE",String.valueOf(SCORE));
         SqlSession sqlSession= JDBCFactory.Instance();
-        int isRight=sqlSession.selectOne("CommentMapper.saveComment",params);
+        int isRight=sqlSession.update("CommentMapper.saveComment",params);
+        sqlSession.commit();
         sqlSession.close();
         if (isRight==1){
-
+            System.out.println("插入成功");
             return true;
         }
+        System.out.println("插入失败");
         return false;
     }
 
@@ -33,7 +34,7 @@ public class CommentMapperImp implements CommentMapper {
 
 
         SqlSession sqlSession= JDBCFactory.Instance();
-        List<Comment> Comment=sqlSession.selectOne("CommentMapper.checkComment",params);
+        List<Comment> Comment=sqlSession.selectList("CommentMapper.checkComment",params);
         sqlSession.close();
         return Comment;
     }
--- src/main/java/org/group11/Patterns/mvc/CommentView.java
+++ src/main/java/org/group11/Patterns/mvc/CommentView.java
@@ -1,2 +1,38 @@
-package org.group11.Patterns.mvc;public class CommentView {
+package org.group11.Patterns.mvc;
+
+import org.group11.Entity.Comment;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Scanner;
+
+public class CommentView {
+    public void displayComments(List<Comment> comments) {
+        // 显示图书评价信息列表
+        for (Comment comment : comments) {
+            System.out.println("BookComment: ");
+            System.out.println("Bookid: " + comment.getBOOK_ID());
+            System.out.println("Content: " + comment.getCOMMENT_CONTENT());
+            System.out.println("Time: " + comment.getCOMMENT_TIME());
+            System.out.println("Score: " + comment.getSCORE());
+            System.out.println("------------------------------");
+        }
+    }
+    public Comment getCommentFromUser(int id) {
+        // 获取用户输入的评价信息
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入您的评价内容：");
+        String content = scanner.nextLine();
+        System.out.println("请输入您的打分：");
+        String score = scanner.nextLine();
+
+        // 创建Comment对象
+        Comment comment = new Comment();
+        comment.setBOOK_ID(id);
+        comment.setCOMMENT_CONTENT(content);
+        comment.setSCORE(Integer.valueOf(score));
+        comment.setCOMMENT_TIME(LocalDateTime.now());
+
+        return comment;
+    }
 }
