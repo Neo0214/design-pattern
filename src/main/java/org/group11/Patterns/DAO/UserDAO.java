a7060587006e473c53b0e65a75e8f652152351ae
zsmx0323 <141441229+zsmx0323@users.noreply.github.com>
2024-01-02 22:13:31 +0800

    Update UserDAO.java

--- src/main/java/org/group11/Patterns/DAO/UserDAO.java
+++ src/main/java/org/group11/Patterns/DAO/UserDAO.java
@@ -1,9 +1,9 @@
 package org.group11.Patterns.DAO;
 
 import org.apache.ibatis.session.SqlSession;
-import org.group11.Patterns.serviceLocator.User;
+import org.group11.Entity.CreateUser;
 
 
 public interface UserDAO {
-    public void createUser(SqlSession sqlSession, User user);
+    public void createUser(SqlSession sqlSession, CreateUser user);
 }
