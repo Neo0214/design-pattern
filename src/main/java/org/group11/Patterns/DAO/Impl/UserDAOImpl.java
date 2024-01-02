dbd7d7f93ecdeb2104635efafc28f8a9c5b7a0fb
zsmx0323 <141441229+zsmx0323@users.noreply.github.com>
2024-01-02 22:12:45 +0800

    Update UserDAOImpl.java

--- src/main/java/org/group11/Patterns/DAO/Impl/UserDAOImpl.java
+++ src/main/java/org/group11/Patterns/DAO/Impl/UserDAOImpl.java
@@ -1,12 +1,12 @@
 package org.group11.Patterns.DAO.Impl;
 
 import org.apache.ibatis.session.SqlSession;
+import org.group11.Entity.CreateUser;
 import org.group11.Patterns.DAO.UserDAO;
-import org.group11.Patterns.serviceLocator.User;
 
 
 public class UserDAOImpl implements UserDAO {
-    public void createUser(SqlSession sqlSession, User user) {
+    public void createUser(SqlSession sqlSession, CreateUser user) {
         if (sqlSession != null) {
             sqlSession.insert("UserMapper.insertUser", user);
         } else {
